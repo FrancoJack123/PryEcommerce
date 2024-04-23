fc686d371c0959ea932fc8b757e2f58e8b6c5528
FrancoJack123 <jackfrancomoreno15@gmail.com>
2024-04-23 13:01:18 -0500

    Configuracion del trabajo de entorno de N capas

---
+++ PryEcommerce.AplicacionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PryEcommerce.AplicacionWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
